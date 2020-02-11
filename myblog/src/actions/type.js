e520c5a98512816b97b0ae63f775ba4ea2a6a836
manhtien465 <nguyenmtien465@gmail.com>
2020-02-11 14:48:16 +0700

    make it better

--- myblog/src/actions/type.js
+++
@@ -1,4 +0,0 @@
-export const GET_ITEM='GET_ITEM';
-export const ADD_ITEM='ADD_ITEM';
-export const DELETE_ITEM='DELETE_ITEM'
-export const ITEM__LOADING='ITEM__LOADING'
--- myblog/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux';
-import itemReducer from './itemReducer';
-import{getItems} from '../actions/itemAction'
-export default combineReducers({
-  itemReducer:itemReducer
-})
