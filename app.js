7131db49862a3148dfef6d65d2b42f928ed828a2
manhtien465 <nguyenmtien465@gmail.com>
2020-02-11 14:43:26 +0700

    laij xem nao

--- app.js
+++ app.js
@@ -59,5 +59,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
+if(process.env.NODE_ENV ==='production'){
+  app.use(express.static('myblog/build'));
+  app.get('*',(req,res)=>{
+res.sendFile(path.resolve(__dirname,'myblog','build','index.html'))
+  })
+}
 module.exports = app;
